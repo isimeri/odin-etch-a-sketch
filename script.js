17bdfe6f18bedfc6d35b1c1804423ab7170f1642
isimeri <loge.lederun@gmail.com>
2023-10-15 00:36:19 +0300

    implemented board resizing function

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const board = document.querySelector(".draw-board");
 const boardSizeSlider = document.querySelector('#board-size');
 const resizeBtn = document.querySelector("#resize-board-btn");
+let boardCellsArr = [];
 
 function resizeBoard(){
     const size = boardSizeSlider.value;
-    console.log(size);
+    const cellSize = 800 / size;
+    boardCellsArr = [];
+    board.innerHTML = '';
+    for(let i = 0; i < size*size; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.height = `${cellSize}px`;
+        cell.style.width = `${cellSize}px`;
+
+        boardCellsArr.push(cell);
+        board.appendChild(cell);
+    }
 }
 
 resizeBtn.addEventListener("click", resizeBoard);
